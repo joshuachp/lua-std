ecaada39570ecb79ed84844c2bad5c41c631bc3d
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2021-09-07 09:43:50 +0200

    feat(lib,fs): initialize modules and read_dir fn

---
+++ src/fs.rs
@@ -0,0 +1,27 @@
+//! Module for file system utilities
+use std::fs;
+
+use mlua::prelude::*;
+
+/// Read the content of a directory returning a string of the file names
+fn read_dir(lua: &Lua, path: String) -> LuaResult<LuaTable> {
+    let content: Vec<String> = fs::read_dir(path)?
+        .map(|entry| {
+            entry
+                .expect("Error reading file")
+                .file_name()
+                .to_str()
+                // This shouldn't happen since the OS sting is from the directory name
+                .expect("Invalid unicode in path")
+                .to_string()
+        })
+        .collect();
+    lua.create_sequence_from(content)
+}
+
+#[mlua::lua_module]
+pub fn fs(lua: &Lua) -> LuaResult<LuaTable> {
+    let module = lua.create_table()?;
+    module.set("read_dir", lua.create_function(read_dir)?)?;
+    Ok(module)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! Lua is missing a std library for common function, this is a gradula implementation in rust of ffi functions you don't want ot write twice.
+
+use mlua::prelude::*;
+
+pub mod fs;
+
+#[mlua::lua_module]
+fn lua_std(lua: &Lua) -> LuaResult<LuaTable> {
+    let module = lua.create_table()?;
+    module.set("fs", fs::fs(lua)?)?;
+    Ok(module)
 }
